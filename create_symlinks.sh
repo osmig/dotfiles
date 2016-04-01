60e9b69fe32ec9b739ceb252cb6e0a756efe0466
Carl Frendin <carl.frendin@gmail.com>
2016-04-01 11:53:08 +0200

    create_symlinks.sh now works

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
-cd ~/dotfiles
+
+#arg1: the dotfile to be linked
+function create_link_to_home_from_dotfiles {
+    echo "Creating new symbolic link..."
+    ln -s "$HOME/dotfiles/$1" "$HOME/$1"
+    echo "successfully created."
+}
+
+#arg1: the dotfile link to be deleted
+function remove_old_link_if_exists {
+    #if symbolic link
+    if [ -L "$HOME/$1" ]; then
+        echo "Removing old symlink $HOME/$1 => $(readlink "$HOME/$1")"
+        rm "$HOME/$1" #remove old symlink if there is any
+    fi
+}
+
 SUCCESS=0
 echo "~/dotfiles/.*rc is:" ~/dotfiles/.*rc
-for file in  ~/dotfiles/.*rc; do
-    echo "Creating symlink for $file"
-    local symfile=$(basename $file)
+for file in  $HOME/dotfiles/.*rc; do
+    echo ""
+    basefile=$(basename $file)
+    echo "Creating symlink for $basefile"
+    echo "basefile=$basefile"
+    remove_old_link_if_exists $basefile
     #if regular file doesn't already exist
-    if [ ! -f "$symfile" ]; then
-        rm "~/$symfile" #remove old symlink if there is any
-        ln -s "~/$symfile" "~/dotfiles/$(basename $file)"
-        echo "successfully created."
+    if [ ! -f "$HOME/$basefile" ]; then
+        create_link_to_home_from_dotfiles $basefile
     else
-        echo "~/$symfile is a real file; remove/rename and rerun this script." 
+        echo "$HOME/$basefile is a real file; remove/rename and rerun this script." 
         SUCCESS=1
     fi
 done
+
+echo ""
+
 if [ $SUCCESS ]; then
-    echo "return SUCCESS."
+    echo "No errors, returned SUCCESS."
 else
-    echo "return FAIL."
+    echo "Error code found, returned FAIL."
 fi
 exit $SUCCESS
 
